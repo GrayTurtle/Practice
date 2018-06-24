5507ae8a9aab4f8cc33eb7ed4947f5e78d3b3d80
John Tran <John.Tran@ou.edu>
2018-06-24 14:59:55 -0500

    asdasda

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social App',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'Ecommerce',
+          category: 'Web Dev'
+        }
+
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My App
+        {/* Passed in this.state.projects from above
+         to Projects.js */}
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
